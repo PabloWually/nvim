3378f7cea805b7c0ed95db74da67d3a48aa8cf04
Pablo Guallichico <pablo.guallichico@outlook.com>
2024-03-25 15:24:45 -0500

    feat: add new features of sintaxis

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -1,45 +1,60 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter-textobjects",
-	},
-	build = ":TSUpdate",
-	event = "VeryLazy",
-	main = "nvim-treesitter.configs",
-	opts = {
-		ensure_installed = {
-			"lua",
-			"luadoc",
-			"typescript",
-			"javascript",
-			"python",
-			"json",
-			"json",
-			"html",
-			"htmldjango",
-			"dockerfile",
-			"css",
-			"gitignore",
-		},
-		highlight = {
-			enabled = true,
-		},
-		indent = {
-			enabled = true,
-		},
-		textobjects = {
-			select = {
-				enabled = true,
-				lookahead = true,
-				keymaps = {
-					["af"] = "@function.outer",
-					["if"] = "@function.inner",
-					["ac"] = "@conditional.outer",
-					["ic"] = "@conditional.inner",
-					["al"] = "@loop.outer",
-					["il"] = "@loop.inner",
-				}
-			}
-		}
-	}
+  "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    "HiPhish/rainbow-delimiters.nvim",
+    "windwp/nvim-ts-autotag",
+  },
+  build = ":TSUpdate",
+  event = "VeryLazy",
+  main = "nvim-treesitter.configs",
+  opts = {
+    highlight = {
+      enable = true,
+      use_languagetree = true,
+    },
+    autotag = {
+      enable = true,
+      filetypes = { "html" , "xml", "tsx", "jsx" },
+    },
+    indent = {
+      enable = true,
+    },
+    rainbow = {
+      enable = true,
+      disable = { 'jsx', 'cpp' },
+      query = 'rainbow-delimiters',
+      strategy = require('rainbow-delimiters').strategy.global,
+    },
+    ensure_installed = {
+      "lua",
+      "luadoc",
+      "typescript",
+      "javascript",
+      "python",
+      "json",
+      "json",
+      "html",
+      "htmldjango",
+      "dockerfile",
+      "css",
+      "gitignore",
+      "prisma",
+      "graphql",
+    },
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true,
+        keymaps = {
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["ac"] = "@conditional.outer",
+          ["ic"] = "@conditional.inner",
+          ["al"] = "@loop.outer",
+          ["il"] = "@loop.inner",
+        }
+      }
+    }
+  }
 }
