88d063e1dd9f9a7f1f7ca5306e5213ba322df633
Pablo Guallichico <pablo.guallichico@outlook.com>
2025-07-29 16:29:36 -0500

    feat(git): enhance git workflow
    - Update fugitive pull to only fetch from the current branch to avoid unintended merges from other branches.
    - Integrate telescope-git-branch to improve branch management, including a new keymap for creating branches.
    - Add the newpaper colorscheme as an option.
    - Remove supermaven plugin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -53,5 +53,10 @@ return {
     lazy = false,
     priority = 1000,
     opts = {},
+  },
+  {
+    "yorik1984/newpaper.nvim",
+    priority = 1000,
+    config = true,
   }
 }
--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -3,7 +3,7 @@ return {
   cmd = { "G", "Git" },
   keys = {
     { "<leader>ga", ":Git fetch --all -p<cr>",             desc = "Git fetch" },
-    { "<leader>gl", ":Git pull<cr>",                       desc = "Git pull" },
+    { "<leader>gl", ":Git pull origin HEAD<cr>",           desc = "Git pull current branch" },
     { "<leader>cc", ":Telescope conventional_commits<CR>", desc = "Git commit" },
   },
 }
