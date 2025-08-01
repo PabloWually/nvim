1f3f21be1b29a4e9a2017b71cb0e8d6ad1f5ad0d
Pablo Guallichico <pablo.guallichico@outlook.com>
2025-08-01 16:46:46 -0500

    refactor: replace nvim-cmp for blink

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,108 +1,48 @@
 return {
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-cmdline',
-      'hrsh7th/cmp-nvim-lua',
-      'hrsh7th/cmp-git',
-      'saadparwaiz1/cmp_luasnip',
-      'L3MON4D3/LuaSnip',
-      "onsails/lspkind.nvim",
+  'saghen/blink.cmp',
+  dependencies = { 'rafamadriz/friendly-snippets' },
+  version = '1.*',
+  opts = {
+    appearance = {
+      use_nvim_cmp_as_default = false,
+      nerd_font_variant = "mono",
     },
-    event = "VeryLazy",
-    config = function()
-      local cmp = require("cmp")
-      local lspkind = require("lspkind")
-      local luasnip = require('luasnip')
-      local cmp_autopairs = require "nvim-autopairs.completion.cmp"
-      vim.opt.completeopt = { "menu", "menuone", "preview" }
-
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
+    completion = {
+      accept = {
+        auto_brackets = {
+          enabled = true,
         },
-        mapping = {
-          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<C-n>"] = cmp.mapping.select_next_item(),
-          ["<C-p>"] = cmp.mapping.select_prev_item(),
-          ["<CR>"] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Insert,
-            select = true,
-          }),
-          ["<c-space>"] = cmp.mapping.complete(),
+      },
+      menu = {
+        draw = {
+          treesitter = { "lsp" },
         },
-        sources = {
-          { name = "nvim_lsp" },
-          { name = "path" },
-          { name = "luasnip" },
-          { name = "buffer" },
-          { name = 'path' },
-        },
-        formatting = {
-          format = lspkind.cmp_format({
-            maxwidth = {
-              menu = 50,
-              abbr = 50,
-            },
-            ellipsis_char = "...",
-          }),
-        },
-        window = {
-          completion = { -- rounded border
-            border = "rounded",
-            scrollbar = true,
-          },
-          documentation = { -- rounded border
-            border = "rounded",
-            scrollbar = "",
-          },
-        },
-      })
+      },
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 200,
+      },
+      ghost_text = {
+        enabled = vim.g.ai_cmp,
+      },
+    },
 
-      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = "" } })
+    signature = { enabled = true },
 
-      -- Set configuration for specific filetype.
-      cmp.setup.filetype('gitcommit', {
-        sources = cmp.config.sources({
-          { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
-        }, {
-          { name = 'buffer' },
-        })
-      })
+    sources = {
+      compat = {},
+      default = { "lsp", "path", "snippets", "buffer" },
+    },
 
-      -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-      cmp.setup.cmdline({ '/', '?' }, {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = 'buffer' },
-        }
-      })
+    cmdline = {
+      enabled = true,
+    },
 
-      -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-      cmp.setup.cmdline(':', {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = 'path' }
-        }, {
-          { name = 'cmdline' }
-        })
-      })
-    end
-  },
-  {
-    "L3MON4D3/LuaSnip",
-    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-    dependencies = { "rafamadriz/friendly-snippets" },
-    build = "make install_jsregexp",
-    config = function()
-      require("luasnip.loaders.from_vscode").lazy_load()
-    end
+    keymap = {
+      preset = "enter",
+      ["<C-y>"] = { "select_and_accept" },
+    },
+    opts_extend = { "sources.default" },
+    fuzzy = { implementation = "prefer_rust_with_warning" }
   }
 }
