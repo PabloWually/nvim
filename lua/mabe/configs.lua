9f1d4c255696524150bc944af00c9f72ed40f910
Pablo Guallichico <pablo.guallichico@outlook.com>
2025-08-05 13:01:49 -0500

    feat(indent): migrate from indent-blankline to snacks
    This commit replaces `indent-blankline.nvim` with the indent feature from `snacks.nvim`.
    
    The `snacks.nvim` plugin is already a dependency, so this change simplifies the plugin setup and reduces the number of plugins.
    
    Key changes:
    - Remove `indent-blankline.nvim` plugin.
    - Enable indent feature in `snacks.nvim`.
    - Move rainbow highlight colors to `lua/utils/icons.lua` to be used by `snacks.nvim`.
    - Update `lazy-lock.json`.

--- lua/mabe/configs.lua
+++ lua/mabe/configs.lua
@@ -15,17 +15,3 @@ vim.api.nvim_create_autocmd("LspAttach", {
   end,
 })
 
-local prev = { new_name = "", old_name = "" } -- Prevents duplicate events
-local Snacks = require "snacks"
-vim.api.nvim_create_autocmd("User", {
-  pattern = "NvimTreeSetup",
-  callback = function()
-    local events = require("nvim-tree-api").events
-    events.subscribe(events.Event.NodeRenamed, function(data)
-      if prev.new_name ~= data.new_name or prev.old_name ~= data.old_name then
-        data = data
-        Snacks.rename.on_rename_file(data.old_name, data.new_name)
-      end
-    end)
-  end,
-})
--- lua/plugins/snaks.lua
+++ lua/plugins/snaks.lua
@@ -1,8 +1,10 @@
+local colors = require("utils.icons").rainbow
 return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
   opts = {
+    indent = { enabled = true, },
     bigfile = { enabled = true },
     input = { enabled = true },
     notifier = { enabled = true },
